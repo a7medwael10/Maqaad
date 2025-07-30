09d306f6209714ce0f8c8ed90b10aeb57b9846bc
awael <ahmed.wm05@gmail.com>
2025-07-30 13:07:53 +0300

    site and dashboard

--- resources/views/filament/admin/logo.blade.php
+++
@@ -1,5 +0,0 @@
-<a target="__blank" href="/">
-    {{-- ->defaultThemeMode(ThemeMode::Light); --}}
-
-    <img style="height: 50px" src="{{ asset('site/images/wihtLogo.png')   }}" alt="Logo">
-</a>
--- resources/views/filament/admin/logoDark.blade.php
+++
@@ -1,5 +0,0 @@
-<a target="__blank" href="/">
-    {{-- ->defaultThemeMode(ThemeMode::Light); --}}
-
-    <img style="height: 50px" src="{{ asset('site/images/darkLogo.png') }}" alt="Logo">
-</a>
